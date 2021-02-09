938bb4d387cd886fa0dcf97910e697502c0ceca1
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-02-09 20:16:20 -0300

    Add basic styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
+import '../stylesheet/Button.css';
 
-const Button = ({ name }) => <div className="button">{name}</div>;
+const Button = ({ name }) => <button type="button" className="Button">{name}</button>;
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
