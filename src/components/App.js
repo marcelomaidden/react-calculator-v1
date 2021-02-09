bfea484ee397a76d0f6c652389f2ae5b25b7fe50
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-02-09 19:52:54 -0300

    Create basic components

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
+import React from 'react';
+import Display from './Display';
+import ButtonPanel from './ButtonPanel';
+
 function App() {
   return (
-    <div className="App">
-      Calculator Project
-    </div>
+    <>
+      <Display />
+      <ButtonPanel />
+    </>
   );
 }
 
---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,14 @@
+import Button from './Button';
+
+const ButtonPanel = () => (
+  <>
+    <div>
+      <Button name="AC" />
+      <Button name="+/-" />
+      <Button name="%" />
+      <Button name="÷" />
+    </div>
+  </>
+);
+
+export default ButtonPanel;
