6ec5fd1586e4f5d5b24e76e6094334ca2f2f6d27
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-02-10 17:29:01 -0300

    Change App to stateful component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
 calculate(1, 2, '-');
 
-const App = () => (
-  <>
-    <Display />
-    <ButtonPanel />
-  </>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <Display />
+        <ButtonPanel />
+      </>
+    );
+  }
+}
 
 export default App;
